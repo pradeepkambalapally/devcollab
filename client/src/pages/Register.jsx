204f4738aba24d47dcf85f62890ba3e6dde2c3a6
pradeepkambalapally <pradeepkambalapally2004@gmail.com>
2026-06-10 22:43:23 +0530

    Add authentication pages and protected routing

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,61 @@
-
+import {useState} from "react";
+import api from "../api/api.jsx";
+import {useNavigate} from "react-router-dom";
 const Register = () => {
+    const [formData, setFormData] = useState({
+        username : "",
+        email : "",
+        password : ""
+    })
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value,
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            await api.post("/auth/register", formData);
+            navigate("/login");
+        }catch(error){
+            console.error(error.response.data);
+        }
+    }
     return (
-        <h1>Register Page</h1>
+        <div>
+            <h2>Create An Account</h2>
+            <form onSubmit = {handleSubmit}>
+                <input
+                   type="text"
+                   name="username"
+                   placeholder="Username"
+                   value={formData.username}
+                   onChange={handleChange}
+                 />
+
+                 <input
+                   type="email"
+                   name="email"
+                   placeholder="Email"
+                   value={formData.email}
+                   onChange={handleChange}
+                 />
+
+                 <input
+                   type="password"
+                   name="password"
+                   placeholder="Password"
+                   value={formData.password}
+                   onChange={handleChange}
+                 />
+
+                 <button>Create Account</button>
+            </form>
+        </div>
+
     )
 }
 
