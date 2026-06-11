c64cbb74acff903480e3ac588e244e23c5361b4a
pradeepkambalapally <pradeepkambalapally2004@gmail.com>
2026-06-11 19:43:12 +0530

    Implement chat messaging and persistent auth

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,34 +1,45 @@
-import {createContext, useState, useContext} from "react";
+    import {createContext, useState, useContext} from "react";
 
 
-const AuthContext = createContext();
+    const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
+    export const AuthProvider = ({children}) => {
 
-    const [user, setUser] = useState(null);
-    const [token, setToken] = useState(null);
-    
-    const login = (userData, token) => {
-        setUser(userData);
-        setToken(token);
-    };
-    const logout = () => {
+        const [user, setUser] = useState(
+        JSON.parse(localStorage.getItem("user")) || null
+        );
+
+        const [token, setToken] = useState(
+        localStorage.getItem("token") || null
+        );
+        
+        const login = (userData, token) => {
+         setUser(userData);
+         setToken(token);
+
+         localStorage.setItem("user", JSON.stringify(userData));
+         localStorage.setItem("token", token);
+        };
+        const logout = () => {
         setUser(null);
         setToken(null);
+
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+    };
+
+        return (
+            <AuthContext.Provider 
+            value = {{
+                user,
+                token,
+                login,
+                logout
+            }}> {children} </AuthContext.Provider>
+        )
     }
 
-    return (
-        <AuthContext.Provider 
-        value = {{
-            user,
-            token,
-            login,
-            logout
-        }}> {children} </AuthContext.Provider>
-    )
-}
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const useAuth = () => {
-    return useContext(AuthContext);
-}
+    // eslint-disable-next-line react-refresh/only-export-components
+    export const useAuth = () => {
+        return useContext(AuthContext);
+    }
