8c36bb5566e91191bc172a617c9ed1a990143703
pradeepkambalapally <pradeepkambalapally2004@gmail.com>
2026-06-21 22:36:33 +0530

    fix: update CORS for production

--- server/server.js
+++ server/server.js
@@ -25,7 +25,9 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173",
+      "https://devcollab-seven.vercel.app",
+    ],
     credentials: true,
   })
 );
@@ -40,7 +42,10 @@ const PORT = process.env.PORT || 3000;
 
 const io = new Server(server, {
   cors : {
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173",
+      "https://devcollab-seven.vercel.app",
+    ],
+    methods: ["GET", "POST"],
     credentials: true,
   }
 });
